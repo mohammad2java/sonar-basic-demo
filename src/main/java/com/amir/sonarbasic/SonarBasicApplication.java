15fd1f2479c1131b7114707c238f064fb409a144
mohd <mohammad2java@gmail.com>
2023-02-02 17:07:50 +0530

    initial commit

---
+++ src/main/java/com/amir/sonarbasic/SonarBasicApplication.java
@@ -0,0 +1,13 @@
+package com.amir.sonarbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amir/sonarbasic/SonarBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amir.sonarbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
